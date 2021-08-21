dd30f7e861ac61b0fdd8c60b92721ed13d40c864
reskybayu <reskybayu234@gmail.com>
2021-08-21 21:41:19 +0700

    DOM Selection

---
+++ script.js
@@ -0,0 +1,17 @@
+//DOM SELECTION
+// getElementById
+
+const judul = document.getElementById('judul');
+judul.style.color = 'red';
+judul.style.backgroundColor = 'blue';
+judul.innerHTML = 'Resky Bayu Andhika';
+
+// document.getElementByTagName () -> HTML Colection
+ const p = document.getElementsByTagName('p');
+
+
+//  for (let i = 0; i < p.length ;i++){
+//     p[i].style.backgroundColor = 'lightblue';
+//  }
+
+const h1 = document.getElementsByTagName('h1');
