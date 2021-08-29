b11af60a207545c9b8d341a1e63534a6735f6cf6
Resky Bayu Andhika <75589544+reskybayu234@users.noreply.github.com>
2021-08-29 18:54:50 +0700

    Add files via upload

---
+++ DOM Traversal/script.js
@@ -0,0 +1,16 @@
+const close = document.querySelectorAll('.close');
+
+// for (let i = 0; i < close.length; i++){
+//     close[i].addEventListener('click',function(e){
+//         // close[i].parentElement.style.display = 'none'
+//         e.target.parentElement.style.display = 'none';
+//     })
+// }
+
+// dengan menggunakan perulangan foreach
+
+close.forEach(function(el){
+    el.addEventListener('click',function(e){
+        e.target.parentElement.style.display = 'none';
+    })
+})
